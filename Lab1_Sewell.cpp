548d18acda5322bb2933c0a0c69de7643afacc81
WSewell-arch <Winston.sewell12@gmail.com>
2026-01-25 22:48:51 -0500

    Add files via upload

---
+++ Lab1_Sewell.cpp
@@ -0,0 +1,23 @@
+
+/**
+ * @file Lab1_wsewell1.cpp
+ * @Winston Sewell
+ * @date 2025-01-15
+ * @brief A simple program to greet the user by name.
+ */
+#include<ioStream>
+using namespace std;
+
+int main()
+{
+    const FirstName, LastName;
+    cout << "Please Enter your First Name: ";
+    cin >> FirstName;
+    cout << "Please Enter your Last Name: ";
+    cin >> LastName;
+    cout >> "Welcome To C++ Programming! ">> FirstName >> LastName>> ;
+
+    return 0;
+
+
+}
